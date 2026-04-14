38d7e2878cc35132d431cd1066c19b9a8e7a375f
Corrado Belmonte <corradino.belmonte@gmail.com>
2026-04-14 23:25:50 +0200

    feat: update dependencies for Bouncy Castle, DNSJava, JSON, and Compose libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,5 +79,5 @@ dependencies {
     implementation(libs.slf4j.simple)
     implementation(libs.play.services.cronet)
     implementation(libs.kwik)
-    implementation("org.bouncycastle:bcprov-jdk18on:1.78.1")
+    implementation(libs.bcprov.jdk18on)
 }
